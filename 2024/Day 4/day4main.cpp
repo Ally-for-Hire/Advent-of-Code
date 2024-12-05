e12b00d060a2d76ab7d6c5535557d1f19d5b4c54
Ally for Hire <zacyandell@email.com>
2024-12-04 18:09:22 -0600

    Finished day 4 part 1

--- 2024/Day 4/day4main.cpp
+++ 2024/Day 4/day4main.cpp
@@ -3,12 +3,14 @@
 #include <string>
 using namespace std;
 
-bool validateOpen (ifstream &file);
+bool validateOpen (ifstream &file); // (Filename)
+bool validateDir (string, int, int, char*, int); // (Whole String, Length, Index, Next Letters, Direction)
+int booltoint (bool); // (Bool)
 
 int main()
 {
     // File Validation
-    ifstream dataFile("test"); 
+    ifstream dataFile("input"); 
     bool open = validateOpen(dataFile);
     if (!open) { cout << "File unable to be opened."; return 0; }
 
@@ -18,22 +20,29 @@ int main()
     // Part 1 Solution
     // ---------------------------------------------------------------------------//
     string wholefile;
-    int length = 0;
+    int length, wholeWords = 0;
+    char nextletters[] = {'M', 'A', 'S'};
 
     while (getline(dataFile, line))
     {
         if (length == 0) { length = line.length(); }
         wholefile += line;
     }
-    
+
     for (int i = 0; i < wholefile.length(); i++)
     {
-        if (i % length == 0 && i > 0) { cout << endl; }
-        cout << wholefile[i];
+        char letter = wholefile[i];
+        if (letter != 'X') { continue; }
+
+        for (int direction = 0; direction <= 7; direction++)
+        {
+            bool check = validateDir(wholefile, length, i, nextletters, direction);
+            wholeWords += booltoint(check);
+        }
+        
+        
     }
-    cout << endl;
-    
-    cout << "The answer to Part 1 is: " << endl;
+    cout << "The answer to Part 1 is: " << wholeWords << endl;
     // ---------------------------------------------------------------------------//
     
     dataFile.clear();
@@ -47,11 +56,49 @@ int main()
     return 0;
 }
 
+int booltoint (bool value) { return value && 1 || 0; }
+
 bool validateOpen (ifstream &file)
 {
     if (!file.is_open())
     {
         return false;
     }
+    return true;
+}
+
+bool checkIndex (string wholefile, int index, char search)
+{
+    if (wholefile[index] == search)
+    {
+        return true;
+    }    
+    return false;
+}
+
+bool validateDir (string wholefile, int length, int index, char nextletters[], int direction)
+{
+    int localIndex = index;
+    int currentRow = index / length;
+
+    for (int x = 0; x < 3; x++)
+    {
+        // Up, Down, Left, Up Left, Down Left, Right, Up Right, Down Right
+        if (direction == 0) { localIndex -= length; currentRow -= 1;}
+        else if (direction == 1) { localIndex += length; currentRow += 1;}
+        else if (direction == 2) { localIndex -= 1; }
+        else if (direction == 3) { localIndex -= length; localIndex -= 1; currentRow -= 1; }
+        else if (direction == 4) { localIndex += length; localIndex -= 1; currentRow += 1; }
+        else if (direction == 5) { localIndex += 1; }
+        else if (direction == 6) { localIndex -= length; localIndex += 1; currentRow -= 1; }
+        else if (direction == 7) { localIndex += length; localIndex += 1; currentRow += 1; }
+        else { return false; }
+
+        if (localIndex < 0 || localIndex > wholefile.length() - 1) { return false; }
+        if (localIndex / length != currentRow) { return false; }
+        
+        if (!checkIndex(wholefile, localIndex, nextletters[x])) { return false; }
+    }
+
     return true;
 }
