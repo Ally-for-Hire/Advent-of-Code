442003ba75188c81f84d6f38a851b8f50c9cf903
Ally for Hire <zacyandell@email.com>
2024-12-04 18:44:20 -0600

    day 4 part 2 done

--- 2024/Day 4/day4main.cpp
+++ 2024/Day 4/day4main.cpp
@@ -3,9 +3,9 @@
 #include <string>
 using namespace std;
 
-bool validateOpen (ifstream &file); // (Filename)
-bool validateDir (string, int, int, char*, int); // (Whole String, Length, Index, Next Letters, Direction)
-int booltoint (bool); // (Bool)
+bool validateOpen (ifstream &file);
+bool validateDir (string, int, int, char*, int, int); 
+int booltoint (bool); 
 
 int main()
 {
@@ -15,19 +15,18 @@ int main()
     if (!open) { cout << "File unable to be opened."; return 0; }
 
     // Global Variables
-    string line;
-
-    // Part 1 Solution
-    // ---------------------------------------------------------------------------//
-    string wholefile;
+    string line, wholefile;
     int length, wholeWords = 0;
-    char nextletters[] = {'M', 'A', 'S'};
 
     while (getline(dataFile, line))
     {
         if (length == 0) { length = line.length(); }
         wholefile += line;
     }
+
+    // Part 1 Solution
+    // ---------------------------------------------------------------------------//
+    char nextletters[] = {'M', 'A', 'S'};
 
     for (int i = 0; i < wholefile.length(); i++)
     {
@@ -36,22 +35,40 @@ int main()
 
         for (int direction = 0; direction <= 7; direction++)
         {
-            bool check = validateDir(wholefile, length, i, nextletters, direction);
+            bool check = validateDir(wholefile, length, i, nextletters, 3, direction);
             wholeWords += booltoint(check);
         }
-        
-        
     }
     cout << "The answer to Part 1 is: " << wholeWords << endl;
     // ---------------------------------------------------------------------------//
     
     dataFile.clear();
     dataFile.seekg(0, ios::beg);
+    wholeWords = 0;
 
     // Part 2 Solution
     // ---------------------------------------------------------------------------//
-    cout << "The answer to Part 2 is: " << endl;
+    char mArray[] = {'M'};
+    char sArray[] = {'S'};
+
+    for (int i = 0; i < wholefile.length(); i++)
+    {
+        char letter = wholefile[i];
+        if (letter != 'A') { continue; }
+
+        // Up, Down, Left, Up Left, Down Left, Right, Up Right, Down Right
+        int checkULDR1 = validateDir(wholefile, length, i, mArray, 1, 3) + validateDir(wholefile, length, i, sArray, 1, 7) == 2;
+        int checkULDR2 = validateDir(wholefile, length, i, sArray, 1, 3) + validateDir(wholefile, length, i, mArray, 1, 7) == 2;
+
+        int checkDLUR1 = validateDir(wholefile, length, i, mArray, 1, 4) + validateDir(wholefile, length, i, sArray, 1, 6) == 2;
+        int checkDLUR2 = validateDir(wholefile, length, i, sArray, 1, 4) + validateDir(wholefile, length, i, mArray, 1, 6) == 2;
+
+        wholeWords += booltoint((checkULDR1 || checkULDR2) && (checkDLUR1 || checkDLUR2));
+    }
+    
+    cout << "The answer to Part 2 is: " << wholeWords << endl;
     // ---------------------------------------------------------------------------//
+
     dataFile.close();
     return 0;
 }
@@ -76,12 +93,12 @@ bool checkIndex (string wholefile, int index, char search)
     return false;
 }
 
-bool validateDir (string wholefile, int length, int index, char nextletters[], int direction)
+bool validateDir (string wholefile, int length, int index, char nextletters[], int arrayLength, int direction)
 {
     int localIndex = index;
     int currentRow = index / length;
 
-    for (int x = 0; x < 3; x++)
+    for (int x = 0; x < arrayLength; x++)
     {
         // Up, Down, Left, Up Left, Down Left, Right, Up Right, Down Right
         if (direction == 0) { localIndex -= length; currentRow -= 1;}
