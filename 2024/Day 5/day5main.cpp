a578cae9ed8b754998a5db162add6fc1dc36ef5b
Ally for Hire <zacyandell@email.com>
2024-12-06 15:10:52 -0600

    day 5 part 2 done

--- 2024/Day 5/day5main.cpp
+++ 2024/Day 5/day5main.cpp
@@ -78,7 +78,81 @@ int main()
 
     // Part 2 Solution
     // ---------------------------------------------------------------------------//
-    cout << "The answer to Part 2 is: " << endl;
+    int newlyValidRules = 0;
+    
+    while (getline(dataFile, line)) // For every line
+    {
+        bool followsRules = true;
+        bool followDefault = true;
+        string lineTake, lineNew;
+        vector<int> fullLine;
+
+        while (getline(ruleFile, rule)) // For every rule
+        {
+            if (followDefault)
+            {
+                lineTake = line;
+            }
+            else
+            {
+                lineTake = lineNew;
+            }
+
+            istringstream ruleStream (rule);
+            istringstream dataStream (lineTake);
+            string beforeStr, afterStr, numberStr;
+            
+            getline(ruleStream, beforeStr, '|');
+            getline(ruleStream, afterStr, '|');
+
+            int before = stoi(beforeStr);
+            int after = stoi(afterStr);
+
+            int indbefore = -1, indafter = -1, index = 0;
+            bool generatefullLine = fullLine.size() == 0;
+
+            while (getline(dataStream, numberStr, ','))
+            {   
+                if (generatefullLine) { fullLine.push_back(stoi(numberStr)); }
+
+                if (stoi(numberStr) == before) { indbefore = index; }
+                else if (stoi(numberStr) == after) { indafter = index; }
+
+                index++;
+            }
+
+            if (indafter < indbefore && (indbefore != -1 && indafter != -1))
+            {
+                lineNew = "";
+                fullLine[indbefore] = after;
+                fullLine[indafter] = before;
+                cout << line << " -> ";
+                for (int i = 0; i < fullLine.size(); i++) 
+                { 
+                    lineNew += to_string(fullLine[i]) + ","; 
+                }
+                cout << lineNew << endl;
+
+                followDefault = false;
+                followsRules = false;
+
+                ruleFile.clear();
+                ruleFile.seekg(0, ios::beg);
+
+                continue;
+            }
+        }
+
+        if (!followsRules)
+        {
+            newlyValidRules += fullLine[fullLine.size() / 2]; 
+        }
+
+        ruleFile.clear();
+        ruleFile.seekg(0, ios::beg);
+    }
+
+    cout << "The answer to Part 2 is: " << newlyValidRules << endl;
     // ---------------------------------------------------------------------------//
     dataFile.close();
     return 0;
